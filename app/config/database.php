fc03f53360ebd247d138003e305bc4e8d68e5dd3
bimo13 <bimo.jalankaki@yahoo.com>
2016-03-10 19:46:19 +0700

    Update database password.

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'db_edutrax',
 			'username'  => 'root',
-			'password'  => 'Administrator321+',
+			'password'  => 'Edutrax321+',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
