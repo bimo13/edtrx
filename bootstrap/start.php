3ffc366257da8ac3decd702c4ea731f3b5d4407a
bimo13 <bimo.jalankaki@yahoo.com>
2016-03-19 21:10:13 +0700

    Update environment setting.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array(getHostname()),
     'production' => array('edutraxapps.com'),
+	'local' => array(getHostname()),
 
 ));
 
