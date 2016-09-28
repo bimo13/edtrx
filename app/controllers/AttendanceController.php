fdb52cdd018e181cbb328284aa3f561a8a6cdac8
M Panji Aryo Bimo <mochamad.panji@inmagine.com>
2016-09-28 13:37:59 +0700

    Update: Attendance detail view.

--- app/controllers/AttendanceController.php
+++ app/controllers/AttendanceController.php
@@ -1,4 +1,6 @@
 <?php
+use DataTables\DataTablesCustom as DatatablesCustom;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AttendanceController extends BaseController {
 
@@ -27,10 +29,11 @@ class AttendanceController extends BaseController {
     }
 
     public function showStudent($student_id, $date = NULL) {
+        $user = Sentry::getUser();
         try {
-            $student = Student::findOrFail($id);
-            $attendances = Attendance::where('student_id', '=', $student_id);
-            return View::make('attendance-student-detail');
+            $student = Student::findOrFail($student_id);
+            $attendances = Attendance::where('student_id', '=', $student_id)->orderBy('date', 'desc');
+            return View::make('attendance-student-detail', compact('user', 'student', 'attendances'));
         } catch (ModelNotFoundException $e) {
             return $this->redirectNotFound();
         }
@@ -64,6 +67,58 @@ class AttendanceController extends BaseController {
         $classes = Attendance::getClassData();
         $students = Attendance::getStudentsData();
         return View::make('new-attendance-form', compact('user', 'classes', 'students'));
+    }
+
+    // END OF RESTful ROUTING
+    // BEGIN DATA-TRANSACTION ROUTING (AJAX NECESSITIES)
+    public function getDetail($id) {
+        $attendances = Attendance::getDetail($id);
+
+        // Begin constructing data for datatables
+        $datatable = DatatablesCustom::of($attendances);
+        $datatable->remove_column('id');
+        $datatable->remove_column('time_arrive');
+        $datatable->remove_column('student_id');
+        $datatable->remove_column('description');
+        $datatable->edit_column('attendance', function($obj) {
+            if($obj->attendance && $obj->time_arrive == "08:00:00") {
+                $ret = "<i class=\"text-success glyphicon glyphicon-ok\"></i>";
+            } elseif($obj->attendance && $obj->time_arrive != "08:00:00") {
+                $ret = "<i class=\"text-warning glyphicon glyphicon-ok\"></i>";
+            } else {
+                $ret = "<i class=\"text-danger glyphicon glyphicon-remove\"></i>";
+            }
+            
+            return $ret;
+        });
+        $datatable->edit_column('lateness', function($obj) {
+            if($obj->attendance && $obj->time_arrive != "08:00:00") {
+                $ret = "<span class=\"text-danger\">" . date('H:i', strtotime($obj->time_arrive)) . "</span>";
+            } else {
+                $ret = "<i class=\"text-info glyphicon glyphicon-minus\"></i>";
+            }
+            
+            return $ret;
+        });
+        $datatable->edit_column('apparel', function($obj) {
+            if($obj->description == "" || $obj->description == NULL) {
+                $ret = "<i class=\"text-success glyphicon glyphicon-ok\"></i>";
+            } else {
+                $ret = "<i class=\"text-danger glyphicon glyphicon-remove\"></i>";
+            }
+            
+            return $ret;
+        });
+        $datatable->edit_column('notes', function($obj) {
+            if($obj->description == "" || $obj->description == NULL) {
+                $ret = "<i class=\"text-info glyphicon glyphicon-minus\"></i>";
+            } else {
+                $ret = $obj->description;
+            }
+            
+            return $ret;
+        });
+        return $datatable->make();
     }
 
 }
--- app/views/attendance-student-detail.blade.php
+++ app/views/attendance-student-detail.blade.php
@@ -1,1 +1,47 @@
 @extends('dashboard')
+
+@section('title')
+    Attendance &bull; {{ $student->first_name }} {{ $student->last_name }}
+@stop
+
+@section('styles')
+    {{ HTML::style('assets/css/datatables/jquery.dataTables.css') }}
+    {{ HTML::style('assets/css/custom/attendance-detail.css') }}
+@stop
+
+@section('main-content')
+
+    <div class="col-sm-12 attendance-title">
+        Attendance Report
+    </div>
+    <div class="col-sm-12 attendance-details">
+        <p>Student No: <b>{{ $student->student_no }}</b></p>
+        <p>Student Name: <b>{{ $student->first_name }} {{ $student->last_name }}</b></p>
+    </div>
+
+    <div class="clear"></div>
+
+    <div class="col-sm-12">
+        <table id="datatables" class="table table-bordered table-condensed table-striped">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Attendance</th>
+                    <th>Lateness</th>
+                    <th>Apparel</th>
+                    <th>Notes</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+
+@stop
+
+@section('scripts')
+<script type="text/javascript">
+    var base_url = "{{ url() }}";
+    var student_id = "{{ $student->id }}";
+</script>
+{{ HTML::script('assets/js/datatables/jquery.dataTables.min.js') }}
+{{ HTML::script('assets/js/custom/attendance-detail.js') }}
+@stop
